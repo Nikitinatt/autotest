fbce9f15c9065ef0ee89eb2acc743b2d10ed7461
Dmitrij Serov <idmitryserov@gmail.com>
2023-07-08 16:59:20 +0300

    added login test

--- logintest.py
+++ logintest.py
@@ -1,4 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 URL = "https://www.saucedemo.com/"
 LOGIN = "standard_user"
 PASSWORD = "secret_sauce"
 
+
+def get_driver():
+    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+def open_url(driver, url):
+    driver.get(url)
+
+
+def get_element(driver, id):
+    return driver.find_element(By.ID, id)
+
+
+def click_element(driver, id):
+    element = get_element(driver=driver, id=id)
+    element.click()
+
+
+def set_value(driver, id, value):
+    element = get_element(driver=driver, id=id)
+    element.send_keys(value)
+
+
+def login(login, password):
+    set_value(driver=driver, id="user-name", value=login)
+    set_value(driver=driver, id="password", value=password)
+    click_element(driver=driver, id="login-button")
+
+
+driver = get_driver()
+open_url(driver=driver, url=URL)
+login(login=LOGIN, password=PASSWORD)
+
+driver.quit()
